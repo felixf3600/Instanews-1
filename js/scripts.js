edb0f5b1ec5ed77bb25348baf118b21d8777cd24
DexterNeuf <dxneufeld14@gmail.com>
2020-02-07 14:19:03 -0800

    added fix for catagories with less than 12 articles ie: fashion

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const selector = document.querySelector('select');
     let articleFlex = document.getElementsByClassName("article-flex");
 
-    selector.addEventListener('change', (event) => {
+    selector.addEventListener('change', () => {
 
         $.ajax({
             method: 'GET',
@@ -10,12 +10,13 @@ document.addEventListener('DOMContentLoaded', () => {
         })
             .done(function (data) {
                 $('.article-flex').empty();
-                for (let i = 0; i < 12; i++) {
+                for (let i = 0; i < 12 && i < data.results.length; i++) {
                     let newArticle = document.createElement("article");
                     let newDescription = document.createElement("p");
                     newArticle.style = `background-image: url(${data.results[i].multimedia[3].url})`
                     newDescription.innerText = `${data.results[i].abstract}`
                     newArticle.append(newDescription);
+                    console.log(data.results.length);
                     articleFlex[0].appendChild(newArticle);
 
                 }

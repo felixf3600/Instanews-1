a466f4d6bf9690bc3f6762306ff639c6d718c4fb
DexterNeuf <dxneufeld14@gmail.com>
2020-02-07 11:48:27 -0800

    added funconality for getting stories images and description from api and populating it in html

--- js/scripts.js
+++ js/scripts.js
@@ -8,16 +8,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
     $.ajax({
         method: 'GET',
-        url: 'https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=rJvMd2jziFfbgyfxJIe0nzpeT0yrb69c'
+        url: 'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=rJvMd2jziFfbgyfxJIe0nzpeT0yrb69c'
     })
         .done(function (data) {
-            console.log(data.results.length);
-            for (i = 0; i < 12; i++) {
-                console.log(data.results[i])
+            console.log(data);
+            let articleFlex = document.getElementsByClassName("article-flex");
+            for (let i = 0; i < 12; i++) {
+                let newArticle = document.createElement("article");
+                let newDescription = document.createElement("p");
+                newArticle.style = `background-image: url(${data.results[i].multimedia[3].url})`
+                newDescription.innerText = `${data.results[i].abstract}`
+                newArticle.append(newDescription);
+                articleFlex[0].appendChild(newArticle);
+
             }
-            const articleFlex = document.getElementsByClassName("article-flex");
-            articleFlex[0].children[0].firstElementChild.src = `${data.results[1].multimedia[2].url}`
-            console.log(articleFlex[0].children[0].firstElementChild.src);
+
+
+
         })
 
 

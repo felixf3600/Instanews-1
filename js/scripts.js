95667d6f91feccc6c702869c435ae78a5fa8db74
DexterNeuf <dxneufeld14@gmail.com>
2020-02-07 10:31:14 -0800

    applyed image for api into a DOM element

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,19 @@ document.addEventListener('DOMContentLoaded', () => {
         container.style.display = "block";
     });
 
+    $.ajax({
+        method: 'GET',
+        url: 'https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=rJvMd2jziFfbgyfxJIe0nzpeT0yrb69c'
+    })
+        .done(function (data) {
+            console.log(data.results.length);
+            for (i = 0; i < 12; i++) {
+                console.log(data.results[i])
+            }
+            const articleFlex = document.getElementsByClassName("article-flex");
+            articleFlex[0].children[0].firstElementChild.src = `${data.results[1].multimedia[2].url}`
+            console.log(articleFlex[0].children[0].firstElementChild.src);
+        })
 
 
 

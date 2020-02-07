88a5c2ce79fa0c57338a7bc1c021a812eb293436
DexterNeuf <dxneufeld14@gmail.com>
2020-02-07 14:00:47 -0800

    clears article section on new selection

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const myButton = document.querySelector('button');
-    let container = document.getElementById('container');
-    myButton.addEventListener('click', function () {
+    const selector = document.querySelector('select');
+    let articleFlex = document.getElementsByClassName("article-flex");
 
-        container.style.display = "block";
+    selector.addEventListener('change', (event) => {
+
+        $.ajax({
+            method: 'GET',
+            url: `https://api.nytimes.com/svc/topstories/v2/${event.target.value}.json?api-key=rJvMd2jziFfbgyfxJIe0nzpeT0yrb69c`
+        })
+            .done(function (data) {
+                $('.article-flex').empty();
+                for (let i = 0; i < 12; i++) {
+                    let newArticle = document.createElement("article");
+                    let newDescription = document.createElement("p");
+                    newArticle.style = `background-image: url(${data.results[i].multimedia[3].url})`
+                    newDescription.innerText = `${data.results[i].abstract}`
+                    newArticle.append(newDescription);
+                    articleFlex[0].appendChild(newArticle);
+
+                }
+
+
+
+            })
     });
 
-    $.ajax({
-        method: 'GET',
-        url: 'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=rJvMd2jziFfbgyfxJIe0nzpeT0yrb69c'
-    })
-        .done(function (data) {
-            console.log(data);
-            let articleFlex = document.getElementsByClassName("article-flex");
-            for (let i = 0; i < 12; i++) {
-                let newArticle = document.createElement("article");
-                let newDescription = document.createElement("p");
-                newArticle.style = `background-image: url(${data.results[i].multimedia[3].url})`
-                newDescription.innerText = `${data.results[i].abstract}`
-                newArticle.append(newDescription);
-                articleFlex[0].appendChild(newArticle);
-
-            }
-
-
-
-        })
 
 
 

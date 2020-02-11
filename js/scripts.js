75df2ae7ac10693808751d07959dc18e72c0f5dd
DexterNeuf <dxneufeld14@gmail.com>
2020-02-11 13:38:03 -0800

    added while loop in the article poplatorloop to skip articles with no images

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const selector = document.querySelector('select');
     let articleFlex = document.getElementsByClassName("article-flex");
     let header = document.querySelector('header');
+    let image = document.querySelector('img');
+    let articleNoImgCounter = 0
 
     selector.addEventListener('change', () => {
 
@@ -12,22 +14,24 @@ document.addEventListener('DOMContentLoaded', () => {
             .done(function (data) {
                 $('.article-flex').empty();
                 for (let i = 0; i < 12 && i < data.results.length; i++) {
+                    while (data.results[i].multimedia === undefined) {
+                        articleNoImgCounter++
+                    }
                     let newArticle = document.createElement("article");
                     let newDescription = document.createElement("p");
                     let newLink = document.createElement("a");
-                    newArticle.style = `background-image: url(${data.results[i].multimedia[3].url})`
-                    newDescription.innerText = `${data.results[i].abstract}`
-                    newLink.setAttribute("href", `${data.results[i].short_url}`)
+                    newArticle.style = `background-image: url(${data.results[i + articleNoImgCounter].multimedia[3].url})`
+                    newDescription.innerText = `${data.results[i + articleNoImgCounter].abstract}`
+                    newLink.setAttribute("href", `${data.results[i + articleNoImgCounter].short_url}`)
                     newArticle.append(newLink);
                     newLink.append(newDescription);
                     articleFlex[0].appendChild(newArticle);
+
                 }
-
-
-
             })
             .always(function () {
-                header.style = "height: auto;"
+                header.classList.add("clicked");
+                image.classList.add("clicked");
 
             })
 
